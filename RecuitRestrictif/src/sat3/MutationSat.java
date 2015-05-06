855a832d4246bd096e342c8edf0d6a8ab6b64d57
pierrebergesupelec <pierrebergesupelec@gmail.fr>
2015-05-06 13:20:26 +0200

    Calcul de deltaEpot sur SAT
    Auteur : Baptiste

--- RecuitRestrictif/src/sat3/MutationSat.java
+++ RecuitRestrictif/src/sat3/MutationSat.java
@@ -12,7 +12,7 @@ public class MutationSat extends IMutation{
 	public MutationSat(EtatSat e){
 		int n = e.getNbxi();
 		int rand = (int) (n * Math.random()); 
-		boolean b = !((ElementSat)e.getListe().get(rand)).getassignation();//On inverse le booléen
+		boolean b = !((ElementSat)e.getListe().get(rand)).getassignation();//On inverse le boolÃ©en
 		ElementSat enext = new ElementSat(((ElementSat)e.getListe().get(rand)).getxi(),b);
 		MutationSatElementaire msat = new MutationSatElementaire(enext,rand);
 		ArrayList<MutationElementaire> l = new ArrayList<MutationElementaire>();
@@ -21,8 +21,23 @@ public class MutationSat extends IMutation{
 	}
 	@Override
 	public double calculerdeltaEp(Probleme p, Etat e) {
-		// TODO Auto-generated method stub
-		return 0;
+		MutationSatElementaire mut= (MutationSatElementaire) this.listeMutations.get(0);
+		ElementSat elt =(ElementSat) mut.getElement();
+		int cpt=0;
+		for(Minterme m : elt.getMintermes() ){
+			boolean preced=m.is();
+			int i =m.getIndex(elt);
+			m.calque[i]=-m.calque[i];
+			if(m.is()&&(preced==false)){
+				cpt--;
+			}
+			if(preced && (m.is()==false)){
+				cpt++;
+			}
+			m.calque[i]=-m.calque[i];//reset a la valeur initiale du calque
+			
+		}
+		return cpt;
 	}
 
 	@Override
@@ -36,7 +51,7 @@ public class MutationSat extends IMutation{
 		EtatSat esat = (EtatSat) e;
 		int n = esat.getNbxi();
 		int rand = (int) (n * Math.random()); 
-		boolean b = !((ElementSat)esat.getListe().get(rand)).getassignation();//On inverse le booléen
+		boolean b = !((ElementSat)esat.getListe().get(rand)).getassignation();//On inverse le boolÃ©en
 		ElementSat enext = new ElementSat(((ElementSat)esat.getListe().get(rand)).getxi(),b);
 		MutationSatElementaire msat = new MutationSatElementaire(enext,rand);
 		ArrayList<MutationElementaire> l = new ArrayList<MutationElementaire>();
