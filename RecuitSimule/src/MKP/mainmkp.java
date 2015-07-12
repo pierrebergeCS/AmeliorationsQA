cb1a09c0a6ac4c57e7b7703b09ca34a75d6623ee
BaptCav <Baptiste.cavarec@orange.fr>
2015-07-12 11:47:38 +0200

    Au cas ou on transpose
    parce que pour un informaticien mxn et nxm c'est pareil

--- RecuitSimule/src/MKP/mainmkp.java
+++ RecuitSimule/src/MKP/mainmkp.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class mainmkp {
 
 	public static void main(String[] args) throws IOException {
-		Instance i= Translator.traduit("C:/Users/Baptiste/Desktop/RecuitQuantique/sac94/weish/weish01.dat");
-		System.out.println(i.nombreObjets);
+		Instance i= TransposeLator.traduit("C:/Users/Baptiste/Desktop/RecuitQuantique/sac94/weish/weish01.dat");
+		System.out.println(i.getObj()[2].getWeight()[1]);
 	}
 
 }
