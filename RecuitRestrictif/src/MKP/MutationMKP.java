bd7cae955297a23ab817720f7a45b0fb12ddf430
BaptCav <Baptiste.cavarec@orange.fr>
2015-07-12 18:50:18 +0200

    deltaEC est fait et la mut es adaptée a la forme recuit Q

--- RecuitRestrictif/src/MKP/MutationMKP.java
+++ RecuitRestrictif/src/MKP/MutationMKP.java
@@ -8,88 +8,206 @@ import mutation.IMutation;
 import mutation.MutationElementaire;
 
 public class MutationMKP extends IMutation {
-	
+	int indice;//indice de ce qu'on ajoute
+	int indice2;//indice de ce qu'on enleve
+	int typeOfMut; //+1 si on ajoute, 0 si on échange, -1 si on retire
 	public MutationMKP(Remplissage r){
 		int n = r.getListe().size();//nombre d'objets
-		ArrayList<MutationElementaire> l = new ArrayList<MutationElementaire>();
-		MutationElementaireMKP me = new MutationElementaireMKP(0);
 		boolean b = false;
-		while (!b){
-			int k = (int) (Math.random()*n);
-			me = new MutationElementaireMKP(k);
-			l.add(me);
-			
-			//vérifie le respect des contraintes
-			ElementMKP elt = (ElementMKP) r.getListe().get(k);
-			if (!elt.getAppartenance()){
-				//Cas où on ajoute
-				int index = 0;
-				while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] + elt.getObjet().getWeight()[index] <= r.getInstance().getCapacite()[index])){
-					index++;
-				}
-				b = index==r.getInstance().getNombreSacs();
+		//On essaie d'abord d'ajouter
+		int k = -1;
+		while ( k < 0 || ((ElementMKP) r.getListe().get(k)).getAppartenance()){
+			k = (int) (Math.random()*n);
+		}
+		//vérifie le respect des contraintes
+		ElementMKP elt = (ElementMKP) r.getListe().get(k);
+		int index = 0;
+		while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] + elt.getObjet().getWeight()[index] <= r.getInstance().getCapacite()[index])){
+			index++;
+		}
+		b = index==r.getInstance().getNombreSacs();
+		if (b){
+			this.indice = k;
+			this.typeOfMut = 1;
+		} else {
+			//si on y arrive pas, on tente d'échanger avec un autre
+			boolean b2 = false;
+			int k2 = -1;
+			while ( k2 < 0 || !((ElementMKP) r.getListe().get(k2)).getAppartenance()){
+				k2 = (int) (Math.random()*n);
+			}
+			ElementMKP elt2 = (ElementMKP) r.getListe().get(k2);
+			index = 0;
+			while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] + elt.getObjet().getWeight()[index] - elt2.getObjet().getWeight()[index] <= r.getInstance().getCapacite()[index])){
+				index++;
+			}
+			b2 = index==r.getInstance().getNombreSacs();
+			if (b2) {
+				this.indice = k;
+				this.indice2 = k2;
+				this.typeOfMut = 0;
 			} else {
-				//Cas où on retire
-				int index = 0;
-				while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] - elt.getObjet().getWeight()[index] >= 0)){
-					index++;
-				}
-				b = index==r.getInstance().getNombreSacs();
+				//et enfin, si c'est pas possible on va tenter de retirer k2 (nécessaire si on souhaite réduire le nb d'objets dans le sac)
+				this.indice2 = k2;
+				this.typeOfMut = -1;
+			}
+		}
+	}
+	
+	public int getType(){
+		return this.typeOfMut;
+	}
+	
+	public int getIndice(){
+		return this.indice;
+	}
+	
+	public int getIndice2(){
+		return this.indice2;
+	}
+	
+
+	public void faire(Probleme p,Etat e) {
+		Remplissage r = (Remplissage) e;
+		int m = r.getInstance().getNombreSacs();
+		if (this.typeOfMut == 1){
+			ElementMKP elt =(ElementMKP) r.getListe().get(this.indice);
+			elt.changeAppartenance();
+			for(int i = 0; i < m; i++){
+				r.getPoids()[i] += elt.getObjet().getWeight()[i];
+			}
+		}
+		if (this.typeOfMut == -1){
+			ElementMKP elt2 = (ElementMKP) r.getListe().get(this.indice2);
+			elt2.changeAppartenance();
+			for(int i = 0; i < m; i++){
+				r.getPoids()[i] -= elt2.getObjet().getWeight()[i];
+			}
+		}
+		if (this.typeOfMut == 0){
+			ElementMKP elt = (ElementMKP) r.getListe().get(this.indice);
+			ElementMKP elt2 =(ElementMKP)  r.getListe().get(this.indice2);
+			elt.changeAppartenance();
+			elt2.changeAppartenance();
+			for(int i = 0; i < m; i++){
+				r.getPoids()[i] += elt.getObjet().getWeight()[i] - elt2.getObjet().getWeight()[i];
 			}
 		}
 		
-		this.listeMutations = l;
+		
 	}
 
-	@Override
-	public double calculerdeltaEp(Probleme p, Etat e) {
+	public double calculerdeltaEp(Probleme p,Etat e) {
 		Remplissage r = (Remplissage) e;
-		ElementMKP elt = (ElementMKP) r.getListe().get(this.listeMutations.get(0).getIndice());
-		if (elt.getAppartenance()){
+		if (this.typeOfMut == 1){
+			ElementMKP elt =(ElementMKP) r.getListe().get(this.indice);
 			return -elt.getObjet().getUtility();
-		} else {
-			return elt.getObjet().getUtility();
+		} 
+		if (this.typeOfMut == -1){
+			ElementMKP elt2 =(ElementMKP) r.getListe().get(this.indice2);
+			return elt2.getObjet().getUtility();
 		}
-	}
-
-	@Override
-	public double calculerdeltaSpins(Probleme p, Etat e) {
-		// TODO Auto-generated method stub
+		if (this.typeOfMut == 0){
+			ElementMKP elt =(ElementMKP) r.getListe().get(this.indice);
+			ElementMKP elt2 =(ElementMKP) r.getListe().get(this.indice2);
+			return elt2.getObjet().getUtility()-elt.getObjet().getUtility();
+		} 
 		return 0;
 	}
 
-	@Override
-	public void maj(Probleme p, Etat e) {
+
+	public void maj(Probleme p,Etat e) {
 		Remplissage r = (Remplissage) e;
 		int n = r.getListe().size();//nombre d'objets
-		ArrayList<MutationElementaire> l = new ArrayList<MutationElementaire>();
-		MutationElementaireMKP me = new MutationElementaireMKP(0);
 		boolean b = false;
-		while (!b){
-			int k = (int) (Math.random()*n);
-			me = new MutationElementaireMKP(k);
-			l.add(me);
-			
-			//vérifie le respect des contraintes
-			ElementMKP elt = (ElementMKP) r.getListe().get(k);
-			if (!elt.getAppartenance()){
-				//Cas où on ajoute
-				int index = 0;
-				while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] + elt.getObjet().getWeight()[index] <= r.getInstance().getCapacite()[index])){
-					index++;
-				}
-				b = index==r.getInstance().getNombreSacs();
+		//On essaie d'abord d'ajouter
+		int k = -1;
+		while ( k < 0 || ((ElementMKP) r.getListe().get(k)).getAppartenance()){
+			k = (int) (Math.random()*n);
+		}
+		//vérifie le respect des contraintes
+		ElementMKP elt = (ElementMKP) r.getListe().get(k);
+		int index = 0;
+		while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] + elt.getObjet().getWeight()[index] <= r.getInstance().getCapacite()[index])){
+			index++;
+		}
+		b = index==r.getInstance().getNombreSacs();
+		if (b){
+			this.indice = k;
+			this.typeOfMut = 1;
+		} else {
+			//si on y arrive pas, on tente d'échanger avec un autre
+			boolean b2 = false;
+			int k2 = -1;
+			while ( k2 < 0 || !((ElementMKP) r.getListe().get(k2)).getAppartenance()){
+				k2 = (int) (Math.random()*n);
+			}
+			ElementMKP elt2 = (ElementMKP) r.getListe().get(k2);
+			index = 0;
+			while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] + elt.getObjet().getWeight()[index] - elt2.getObjet().getWeight()[index] <= r.getInstance().getCapacite()[index])){
+				index++;
+			}
+			b2 = index==r.getInstance().getNombreSacs();
+			if (b2) {
+				this.indice = k;
+				this.indice2 = k2;
+				this.typeOfMut = 0;
 			} else {
-				//Cas où on retire
-				int index = 0;
-				while ((index < r.getInstance().getNombreSacs()) && (r.getPoids()[index] - elt.getObjet().getWeight()[index] >= 0)){
-					index++;
-				}
-				b = index==r.getInstance().getNombreSacs();
+				//et enfin, si c'est pas possible on va tenter de retirer k2 (nécessaire si on souhaite réduire le nb d'objets dans le sac)
+				this.indice2 = k2;
+				this.typeOfMut = -1;
 			}
 		}
-		
-		this.listeMutations = l;
 	}
 
+
+
+	@Override
+	public double calculerdeltaSpins(Probleme p, Etat e) {
+		double cpt=0;
+		Remplissage r = (Remplissage) e;
+		Remplissage preced=(Remplissage) e.getPrevious();
+		Remplissage next=(Remplissage) e.getNext();
+		if (this.typeOfMut == 1){//cas ou on ajoute juste le obj[indice] au sac
+			
+			if(((ElementMKP)next.getListe().get(this.indice)).getAppartenance()){
+				cpt++;
+			}
+			if(((ElementMKP)preced.getListe().get(this.indice)).getAppartenance()){
+				cpt++;
+			}
+			return cpt;
+		} 
+		if (this.typeOfMut == -1){//on enleve obj[indice2] du sac
+			if(((ElementMKP)next.getListe().get(this.indice2)).getAppartenance()){
+				cpt--;
+			}
+			if(((ElementMKP)preced.getListe().get(this.indice2)).getAppartenance()){
+				cpt--;
+			}
+			return cpt;
+		}
+		if (this.typeOfMut == 0){//cas ou on ajoute le obj[indice] au sac et on enleve obj[indice2]
+			
+			if(((ElementMKP)next.getListe().get(this.indice)).getAppartenance()){
+				cpt++;
+			}
+			if(((ElementMKP)preced.getListe().get(this.indice)).getAppartenance()){
+				cpt++;
+			}
+			
+			if(((ElementMKP)next.getListe().get(this.indice2)).getAppartenance()){
+				cpt--;
+			}
+			if(((ElementMKP)preced.getListe().get(this.indice2)).getAppartenance()){
+				cpt--;
+			}
+		} 
+		return 0;
+	}
+
+
+	
+
+	
 }
